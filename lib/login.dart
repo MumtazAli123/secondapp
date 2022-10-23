6febc5edf232462ef1b218e7af910df293646d0c
Mumtaz <kad.dadu@gmail.com>
2022-10-23 14:57:15 +0500

    updated

--- lib/login.dart
+++ lib/login.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My app"),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text("Hello Karachi"),
+        ),
+      ),
+    ));
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text("Hello Twitter"),
-    ),
-  ));
+  runApp(const FadeAppTest());
+}
+
+class FadeAppTest extends StatelessWidget {
+  const FadeAppTest({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "My App",
+            style: TextStyle(color: Colors.red, fontSize: 30),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 100,
+            color: Colors.red[600],
+            child: const Center(
+              child: Text(
+                "Hello Dart ",
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Hello Sir");
+          },
+          child: const Text("Click"),
+        ),
+      ),
+    );
+  }
 }
